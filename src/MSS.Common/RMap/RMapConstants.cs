99286bb178941146c3dc9ca8438a42561460beca
David Minor <david.minor@live.com>
2021-11-07 18:04:56 -0500

    Updated the ENTIRE_SET_RECTANGLE to use exponent = 0, instead of -1 (i.e. a denominator of 1 vs 1/2)

--- src/MSS.Common/RMap/RMapConstants.cs
+++ src/MSS.Common/RMap/RMapConstants.cs
@@ -9,10 +9,14 @@ namespace MSS.Common
 		static RMapConstants()
 		{
 			// The set goes from x = -2 to 1 and from y = -1 to 1.
-			// Setting the exponent to -1 (i.e, using a factor or 1/2) this is
+			// Setting the exponent to -1 (i.e, using a factor of 1/2) this is
 			// x = -4/2 to 2/2 and y = -2/2 to 2/2
 
-			ENTIRE_SET_RECTANGLE = new RRectangle(-4, 2, -2, 2, -1);
+			// Setting the exponent to 0 (i.e, using a factor of 1) this is
+			// x = -2/1 to 1/1 and y = -1/1 to 1/1
+
+
+			ENTIRE_SET_RECTANGLE = new RRectangle(-2, 1, -1, 1, 0);
 
 		}
 	}
