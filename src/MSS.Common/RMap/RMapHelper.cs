586eb73e5442c4e4cce987ce5e7550caf23d5925
David Minor <david.minor@live.com>
2021-11-07 17:57:14 -0500

    Made further optimizations to the Zoom method and fixed the Divide4 method of the RMapHelper class.

--- src/MSS.Common/RMap/RMapHelper.cs
+++ src/MSS.Common/RMap/RMapHelper.cs
@@ -1,6 +1,5 @@
 ﻿using MSS.Types;
 using System;
-using System.Diagnostics;
 using System.Linq;
 
 namespace MSS.Common
@@ -23,81 +22,86 @@ namespace MSS.Common
 			// Create a new rectangle with its exponent adjusted to support the new precision required in the numerators.
 			RRectangle rectangleWithNewExp;
 
-			var quarterOfXLen = Math.DivRem(curWidth, 4, out long remainderX);
-			var quarterOfYLen = Math.DivRem(curHeight, 4, out long remainderY);
+			// And calculate the amount to adjust the x and y coordinates
+			long adjustmentX;
+			long adjustmentY;
+
+			// First see if both the width and height are even
+			// If even, but not integer multiple of 4 then these halves will become quarters
+			var halfOfXLen = Math.DivRem(curWidth, 2, out long remainderX);
+			var halfOfYLen = Math.DivRem(curHeight, 2, out long remainderY);
 
 			if (remainderX == 0 && remainderY == 0)
 			{
-				// The exponent does not need to be changed, the quarter values are naturaly whole numbers
-				rectangleWithNewExp = rRectangle;
-			}
-			else
-			{
-				var halfOfXLen = Math.DivRem(curWidth, 2, out remainderX);
-				var halfOfYLen = Math.DivRem(curHeight, 2, out remainderY);
+				// Both are even, now let's try 4.
+				var quarterOfXLen = Math.DivRem(curWidth, 4, out remainderX);
+				var quarterOfYLen = Math.DivRem(curHeight, 4, out remainderY);
 
 				if (remainderX == 0 && remainderY == 0)
 				{
-					// The exponent needs to be reduced by 1 (all values are halved)
-					rectangleWithNewExp = new RRectangle(Rebase(rRectangle.Values, -1), rRectangle.Exponent - 1);
-
-					// The half values are now = 1/4 of the original width
-					quarterOfXLen = halfOfXLen;
-					quarterOfYLen = halfOfYLen;
+					// The exponent does not need to be changed, the quarter values are naturaly whole numbers
+					rectangleWithNewExp = rRectangle;
+					adjustmentX = quarterOfXLen;
+					adjustmentY = quarterOfYLen;
 				}
 				else
 				{
-					// The exponent needs to be reduced by 2 (all values are quartered)
-					rectangleWithNewExp = new RRectangle(Rebase(rRectangle.Values, -2), rRectangle.Exponent - 2);
-
-					// The current values can be used
-					quarterOfXLen = curWidth;
-					quarterOfYLen = curHeight;
+					// The exponent needs to be reduced by 1 (all values are halved)
+					rectangleWithNewExp = new RRectangle(Rebase(rRectangle.Values, -1), rRectangle.Exponent - 1);
+					adjustmentX = halfOfXLen;
+					adjustmentY = halfOfYLen;
 				}
+			}
+			else
+			{
+				// The exponent needs to be reduced by 2 (all values are quartered)
+				rectangleWithNewExp = new RRectangle(Rebase(rRectangle.Values, -2), rRectangle.Exponent - 2);
+				adjustmentX = curWidth;
+				adjustmentY = curHeight;
 			}
 
 			//DIAG double x1n = GetVal(rebased.X1, rebased.Exponent);
 
 			RRectangle result = new RRectangle(
-				rectangleWithNewExp.X1 + quarterOfXLen,
-				rectangleWithNewExp.X1 + 3 * quarterOfXLen,
-				rectangleWithNewExp.Y1 + quarterOfYLen,
-				rectangleWithNewExp.Y1 + 3 * quarterOfYLen,
+				rectangleWithNewExp.X1 + adjustmentX,
+				rectangleWithNewExp.X2 - adjustmentX,
+				rectangleWithNewExp.Y1 + adjustmentY,
+				rectangleWithNewExp.Y2 - adjustmentY,
 				rectangleWithNewExp.Exponent
 				);
 
 			return result;
 		}
 
-		//public static long Divide4(long n, int exp, out int newExp)
-		//{
-		//	long result;
+		public static long Divide4(long n, int exp, out int newExp)
+		{
+			long result;
 
-		//	long t = Math.DivRem(n, 4, out long remainder);
+			var half = Math.DivRem(n, 2, out long remainder);
 
-		//	if (remainder == 0)
-		//	{
-		//		result = t;
-		//		newExp = exp;
-		//	}
-		//	else if (remainder == 1)
-		//	{
-		//		result = (t * 4) + 1;
-		//		newExp = exp - 2;
-		//	}
-		//	else if (remainder == 2)
-		//	{
-		//		result = (t * 2) + 1;
-		//		newExp = exp - 1;
-		//	}
-		//	else
-		//	{
-		//		result = (t * 4) + 3;
-		//		newExp = exp - 2;
-		//	}
+			if (remainder == 0)
+			{
+				var quarter = Math.DivRem(n, 4, out remainder);
 
-		//	return result;
-		//}
+				if (remainder == 0)
+				{
+					result = quarter;
+					newExp = exp;
+				}
+				else
+				{
+					result = half;
+					newExp = exp - 1;
+				}
+			}
+			else
+			{
+				result = n;
+				newExp = exp - 2;
+			}
+
+			return result;
+		}
 
 		private static long[] Rebase(long[] vals, int exponentDelta)
 		{
