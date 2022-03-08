57f57e818dd7e2c4b8e291908fb32bda4bd2b544
David Minor <david.minor@live.com>
2022-03-07 23:24:32 -0500

    More code cleanup.

--- src/MSetExplorer/MainWindowViewModel.cs
+++ src/MSetExplorer/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
-﻿using MSS.Types.MSet;
-
+﻿
 namespace MSetExplorer
 {
 	internal class MainWindowViewModel : ViewModelBase, IMainWindowViewModel 
@@ -12,16 +11,16 @@ namespace MSetExplorer
 		public MainWindowViewModel(IJobStack jobStack, IMapDisplayViewModel mapDisplayViewModel)
 		{
 			JobStack = jobStack;
-			JobStack.PropertyChanged += JobStack_PropertyChanged;
+			//JobStack.PropertyChanged += JobStack_PropertyChanged;
 
 			MapDisplayViewModel = mapDisplayViewModel;
 			MapDisplayViewModel.PropertyChanged += MapDisplayViewModel_PropertyChanged;
-			MapDisplayViewModel.MapViewUpdateRequested += MapDisplayViewModel_MapVewUpdateRequested;
+			MapDisplayViewModel.MapViewUpdateRequested += MapDisplayViewModel_MapViewUpdateRequested;
 
 			JobStack.CanvasSize = MapDisplayViewModel.CanvasSize;
 		}
 
-		private void MapDisplayViewModel_MapVewUpdateRequested(object sender, MapViewUpdateRequestedEventArgs e)
+		private void MapDisplayViewModel_MapViewUpdateRequested(object sender, MapViewUpdateRequestedEventArgs e)
 		{
 			JobStack.UpdateMapView(e.TransformType, e.NewArea);
 		}
@@ -36,24 +35,10 @@ namespace MSetExplorer
 
 		#endregion
 
-		#region Event Handlers 
-
-		private void JobStack_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-		{
-			if (e.PropertyName == nameof(IJobStack.CanGoBack))
-			{
-				OnPropertyChanged(nameof(CanGoBack));
-			}
-
-			if (e.PropertyName == nameof(IJobStack.CanGoForward))
-			{
-				OnPropertyChanged(nameof(CanGoForward));
-			}
-		}
-
-		#endregion
-
 		#region Public Properties
+
+		public IMapDisplayViewModel MapDisplayViewModel { get; }
+		public IJobStack JobStack { get; }
 
 		public int Iterations
 		{
@@ -66,14 +51,6 @@ namespace MSetExplorer
 			get => _steps;
 			set { _steps = value; OnPropertyChanged(); }
 		}
-
-		public IMapDisplayViewModel MapDisplayViewModel { get; }
-
-		public IJobStack JobStack { get; }
-
-		public Job CurrentJob => JobStack.CurrentJob;
-		public bool CanGoBack => JobStack.CanGoBack;
-		public bool CanGoForward => JobStack.CanGoForward;
 
 		#endregion
 
