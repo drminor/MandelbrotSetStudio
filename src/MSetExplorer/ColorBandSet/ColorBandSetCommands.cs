fdc660c2390dcbaa0f963ea749b1f320c494666c
David Minor <david.minor@live.com>
2022-04-01 20:50:13 -0400

    Added a context menu to the ColorBandSetUserControl

---
+++ src/MSetExplorer/ColorBandSet/ColorBandSetCommands.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+
+namespace MSetExplorer
+{
+	public static class ColorBandSetCommands
+    {
+        public static readonly RoutedUICommand Edit
+            = new("Edit", "Edit", typeof(ColorBandSetCommands));
+
+        public static readonly RoutedUICommand Cancel
+            = new("Cancel", "Cancel", typeof(ColorBandSetCommands));
+
+        public static readonly RoutedUICommand Commit
+            = new("Commit", "Commit", typeof(ColorBandSetCommands));
+
+        public static readonly RoutedUICommand Delete
+            = new("Delete", "Delete", typeof(ColorBandSetCommands));
+
+        public static readonly RoutedUICommand Insert
+            = new("Insert", "Insert", typeof(ColorBandSetCommands));
+
+        public static readonly RoutedUICommand Apply
+            = new("Apply", "Apply", typeof(ColorBandSetCommands));
+
+        public static readonly RoutedUICommand Settings
+            = new("Settings", "Settings", typeof(ColorBandSetCommands));
+    }
+}
